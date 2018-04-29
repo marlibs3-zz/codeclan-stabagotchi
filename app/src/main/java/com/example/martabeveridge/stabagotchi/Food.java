79c7ec42fc37beb3850e2ce3f0a286f52eabf827
Marta Beveridge <marta@beveridge.uk>
2018-04-29 10:24:34 +0100

    Added food enum with getters.

---
+++ app/src/main/java/com/example/martabeveridge/stabagotchi/Food.java
@@ -0,0 +1,36 @@
+package com.example.martabeveridge.stabagotchi;
+
+// This enum contains all the different types of food you can feed the pet. The first value is the
+// cost of the food in lv (love points) and the second one is the health it restores in hp.
+
+public enum Food {
+
+    TREAT("Treat", 25, 1),
+    BOWL("Bowl", 50, 5),
+    BIGBOWL("Big bowl", 100, 10),
+    RIBS("Ribs", 125, 15),
+    STEAK("Steak", 150, 20);
+
+    private final String prettyNameOfFood;
+    private final int costOfFood;
+    private final int hpRestoreValue;
+
+    private Food(String prettyNameOfFood, int costOfFood, int hpRestoreValue) {
+        this.prettyNameOfFood = prettyNameOfFood;
+        this.costOfFood = costOfFood;
+        this.hpRestoreValue = hpRestoreValue;
+    }
+
+    public String getPrettyNameOfFood() {
+        return prettyNameOfFood;
+    }
+
+    public int getCostOfFood() {
+        return costOfFood;
+    }
+
+    public int getHpRestoreValue() {
+        return hpRestoreValue;
+    }
+
+}
