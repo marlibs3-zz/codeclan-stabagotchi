cb7265b8273e14d4c1dff86506f094306baea975
Marta Beveridge <marta@beveridge.uk>
2018-05-02 11:35:33 +0100

    Set up override in main activity

--- app/src/main/java/com/example/martabeveridge/stabagotchi/GameActivity.java
+++ app/src/main/java/com/example/martabeveridge/stabagotchi/GameActivity.java
@@ -2,6 +2,8 @@ package com.example.martabeveridge.stabagotchi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -9,65 +11,29 @@ public class GameActivity extends AppCompatActivity {
 
     private Game game;
     private Pet pet;
-    private TextView petNameTextView;
+//  App layout
     private ProgressBar healthBar;
+    private TextView level;
+    private TextView lovePoints;
+    private TextView petName;
+    private ImageButton petButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+//      Start up the game
         healthBar = findViewById(R.id.healthBarID);
+        level = findViewById(R.id.levelTextViewID);
+        lovePoints = findViewById(R.id.lovePointsTextViewID);
+        petName = findViewById(R.id.petNameTextViewID);
+
+//      Load previous status TODO
+
+//      Refresh the screen for health to go down TODO
 
     }
 
 }
 
-//public class GameActivity extends AppCompatActivity {
-//    //Game variables
-//    private Game game;
-//    private Player player;
-//    //Player view
-//    private TextView playerNameTextView;
-//    private ProgressBar anxietyBar;
-//    private ProgressBar sleepBar;
-//    private ProgressBar socialLifeBar;
-//    private ProgressBar moneyBar;
-//    //Card text
-//    private TextView cardNameTextView;
-//    private TextView cardProjectCardTextView;
-//    private TextView cardDescriptionTextView;
-//    private TextView cardFirstOptionTextView;
-//    private TextView cardSecondOptionTextView;
-//    //Database
-//    private GamelogDatabase db;
-//
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_game);
-//
-//        //Initialize player views
-//        playerNameTextView = findViewById(R.id.endPlayerNameId);
-//        anxietyBar = findViewById(R.id.anxietyBarId);
-//        sleepBar = findViewById(R.id.sleepBarId);
-//        socialLifeBar = findViewById(R.id.socialLifeBarId);
-//        moneyBar = findViewById(R.id.moneyBarId);
-//
-//        //Initialize card views
-//        cardNameTextView = findViewById(R.id.endSceneTitleTextViewId);
-//        cardProjectCardTextView = findViewById(R.id.cardProjectTextViewId);
-//        cardDescriptionTextView = findViewById(R.id.winLoseDescriptionTextViewId);
-//        cardFirstOptionTextView = findViewById(R.id.cardFirstOptionTextViewId);
-//        cardSecondOptionTextView = findViewById(R.id.cardSecondOptionTextViewId);
-//
-//        //Load game
-//        game = loadGame();
-//        player = game.getPlayer();
-//
-//        //Refresh the screen
-//        refreshView();
-//
-//    }
